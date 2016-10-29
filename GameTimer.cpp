2b057570e7874f20a4cd3d74bc8d5d639ec9aaf5
lsy12012 <dltkddudz@naver.com>
2016-10-29 10:26:50 +0900

    10월 26일
    박스예제 재생성

---
+++ GameTimer.cpp
@@ -0,0 +1,80 @@
+#include <Windows.h>
+#include "GameTimer.h"
+
+GameTimer::GameTimer()
+	: mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0),
+	  mPausedTIme(0), mPrevTime(0), mCurrTime(0), mStopped(false)
+{
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+	mSecondsPerCount = 1.0 / (double)countsPerSec;
+}
+
+float GameTimer::TotalTime() const
+{
+	if (mStopped)
+		return (float)(((mStopTime - mPausedTIme) - mBaseTime) * mSecondsPerCount);
+
+	else
+		return (float)(((mCurrTime - mPausedTIme) - mBaseTime) * mSecondsPerCount);
+}
+
+float GameTimer::DeltaTime() const
+{
+	return (float)mDeltaTime;
+}
+
+void GameTimer::Reset()
+{
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+	mBaseTime = currTime;
+	mPrevTime = currTime;
+	mStopTime = 0;
+	mStopped = false;
+}
+
+void GameTimer::Start()
+{
+	__int64 startTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+
+	if (mStopped)
+	{
+		mPausedTIme += (startTime - mStopTime);
+
+		mPrevTime = startTime;
+		mStopTime = 0;
+		mStopped = false;
+	}
+}
+
+void GameTimer::Stop()
+{
+	if (!mStopped)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+		mStopTime = currTime;
+		mStopped = true;
+	}
+}
+
+void GameTimer::Tick()
+{
+	if (mStopped)
+	{
+		mDeltaTime = 0.0;
+		return;
+	}
+
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	mCurrTime = currTime;
+
+	if (mDeltaTime < 0.0)
+		mDeltaTime = 0.0;
+}
+
